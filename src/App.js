9beb0e154ca60e5cf273751dc98df590692ab40c
Andre Barnard <25427881+AndreBarnard@users.noreply.github.com>
2019-12-02 21:23:28 +0200

    Update site to under construction

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './Images/PurpleRoseMUA_CapeTown_Makeup_Artist.jpg';
+import logoInstagram from './Images/instagram.jpg';
 import './App.css';
 
 function App() {
@@ -7,16 +8,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>This site is under construction - Coming Soon!!!</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/purplerosemuacpt/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <img src={logoInstagram} className="instagram-logo" alt="instagram" />
         </a>
       </header>
     </div>
