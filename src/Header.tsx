9e9e7c986ace1a464b2d91b86c92e99cfeca53d5
abarnard <abarnard@singular.co.za>
2019-12-11 21:10:18 +0200

    Add header page and styling

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import logo from './Images/PurpleRoseMUA_CapeTown_Makeup_Artist.jpg';
+import './styles/_header.scss'
 
 export interface IAppProps {
 }
@@ -7,7 +9,17 @@ export default class App extends React.Component<IAppProps> {
   public render() {
     return (
       <header>
-        
+         <nav className="main-nav">
+          <div className="logo"><img src={logo} className="header-logo" alt="Purple Rose MUA" /></div>
+          <p className="nav-slogan">Professional Makeup Artist</p>
+            <ul> 
+              <li><a href="#">About</a></li>
+              <li><a href="#">News</a></li>
+              <li><a href="#">Learn More</a></li>
+              <li><a href="#">Contact</a></li>
+              <li className="donate"><a href="#">Donate</a></li>
+            </ul>
+          </nav>
       </header>
     );
   }
