67ad11b679d7e199e551e5deca3707e735c5507c
abarnard <abarnard@singular.co.za>
2020-02-02 11:33:38 +0200

    Update Revultion slider

---
+++ src/revolution/component/Caption.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+
+export interface IAppProps {
+}
+
+function Caption() {
+
+    return <div className="tp-caption   tp-resizeme" 
+    id="slide-407-layer-1" 
+    data-x="['left','left','center','center']" data-hoffset="['0','0','0','0']" 
+    data-y="['top','top','top','top']" data-voffset="['0','0','230','110']" 
+               data-fontsize="['110','90','100','70']"
+   data-lineheight="['100','90','100','70']"
+   data-width="['none','none','697','399']"
+   data-height="none"
+   data-whitespace="['nowrap','nowrap','normal','normal']"
+
+   data-type="text" 
+   data-basealign="slide" 
+   data-responsive_offset="on" 
+
+   data-frames='[{"delay":"+290","split":"chars","splitdelay":0.05,"speed":750,"frame":"0","from":"y:40px;sX:1.5;sY:1.5;opacity:0;fb:20px;","to":"o:1;fb:0;","ease":"Power4.easeOut"},
+   {"delay":"wait","speed":1000,"frame":"999","to":"sX:1;sY:1;opacity:0;fb:10px;","ease":"Power4.easeOut"}]'
+   data-margintop="[0,0,0,0]"
+   data-marginright="[0,0,0,0]"
+   data-marginbottom="[30,20,30,30]"
+   data-marginleft="[0,0,0,0]"
+   data-textAlign="['inherit','inherit','center','center']"
+   data-paddingtop="[0,0,0,0]"
+   data-paddingright="[0,0,40,40]"
+   data-paddingbottom="[0,0,0,0]"
+   data-paddingleft="[0,0,40,40]"
+
+   style={{zIndex: 8, whiteSpace: `nowrap`, fontSize: `110px`, lineHeight: `100px`, fontWeight: 600,
+   color: `rgba(255, 255, 255, 1.00)`, display: `inline-block`, fontFamily:`Poppins`, letterSpacing:`-5px`}}>Beauty Spa </div>
+}
+
+
+export default Caption
