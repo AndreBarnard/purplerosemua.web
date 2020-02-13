9a5e36e6929d1f5365839885e6e2af4cbb307f05
Andre Barnard <25427881+AndreBarnard@users.noreply.github.com>
2020-02-13 12:45:21 +0200

    Update Revultion slider

--- src/revolution/component/RevolutionSlider.tsx
+++ src/revolution/component/RevolutionSlider.tsx
@@ -13,15 +13,15 @@ function RevolutionSlider() {
     data-alias="snowaddon1" data-source="gallery" style={{backgroundColor: `#2d3032`, padding:`0px`}}>
             <div id="rev_slider_149_1" className="rev_slider fullscreenbanner" style={{display:`none`}} data-version="5.4.1">
                 <ul>	
-                    {/* <Slide 
+                    <Slide 
                     id="407" 
-                    img={require('../../Images/main-slider/slider5/slide1.jpg')}
+                    img={require('../../Images/main-slider/slider5/PurpleRoseMUA_Bridal_MakeUp.jpg')}
                     heading="Happily ever after"
-                    subheading="Where happiness is just a phone call away"/> */}
+                    subheading="May your love bloom forever."/>
 
                     <Slide 
                     id="408" 
-                    img={require('../../Images/main-slider/slider5/slide1.jpg')}
+                    img={require('../../Images/main-slider/slider5/PurpleRoseMUA_Photoshoot_MakeUp.jpg')}
                     heading="Photoshoot Makeup"
                     subheading="Take you photoghrapy to the next level."/>
                 </ul>                    
