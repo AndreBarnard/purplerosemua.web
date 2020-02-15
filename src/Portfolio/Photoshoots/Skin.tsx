6ce79903d52668fc1f4f2421ed7da95895b1e1e6
Andre Barnard <25427881+AndreBarnard@users.noreply.github.com>
2020-02-15 13:14:26 +0200

    Skin Portfolio

--- src/Portfolio/Photoshoots/Skin.tsx
+++ src/Portfolio/Photoshoots/Skin.tsx
@@ -13,59 +13,59 @@ function Skin() {
     const heading = "Skin";
 
     return <div className="page-content">
-            <Banner img={require('../../Images/portfolio/photoshoots/blacklips/PurplerRose_PhotoShoot_MakeUp_UrbanDecey_BlackLipstick.jpg')} heading={heading} />
+            <Banner img={require('../../Images/portfolio/photoshoots/skin/PurpleRose_PhotoshootMakeUp_Skin_lauren-44.jpg')} heading={heading} />
             <Breadcrumb heading={heading}/>
                 <div className="section-full p-t80 p-b50">
                     <Pagination 
-                    cat1Heading="Urban Decay Lipstick"
-                    cat2Heading="Closeups"
-                    cat3Heading="Lether Jacket"
-                    cat4Heading="Full Frame"
-                    cat5Heading="Family"/>
+                    cat1Heading="Face"
+                    cat2Heading="Group Face"
+                    cat3Heading="Group "
+                    cat4Heading=""
+                    cat5Heading=""/>
                     <Gallery>
                         <PorfolioImage 
-                        img={require('../../Images/portfolio/photoshoots/blacklips/PurpleRose_PhotoshootMakeUp_UrbanDecay8.jpg')} 
-                        heading="Urban Decay Vice Lipstick"
+                        img={require('../../Images/portfolio/photoshoots/skin/PurpleRose_PhotoshootMakeUp_Skin_lauren-34.jpg')} 
+                        heading="Face"
                         desc=""
                         category="cat-1"/>
                         <PorfolioImage 
-                        img={require('../../Images/portfolio/photoshoots/blacklips/PurpleRose_PhotoshootMakeUp_UrbanDecay1.jpg')} 
-                        heading="Closeup"
+                        img={require('../../Images/portfolio/photoshoots/skin/PurpleRose_PhotoshootMakeUp_Skin_lauren-30.jpg')} 
+                        heading="Face"
+                        desc=""
+                        category="cat-1"/>
+                        <PorfolioImage 
+                        img={require('../../Images/portfolio/photoshoots/skin/PurpleRose_PhotoshootMakeUp_Skin_lauren-21.jpg')} 
+                        heading="Group Face"
                         desc=""
                         category="cat-2"/>
                         <PorfolioImage 
-                        img={require('../../Images/portfolio/photoshoots/blacklips/PurpleRose_PhotoshootMakeUp_UrbanDecay2.jpg')} 
-                        heading="Closeup"
-                        desc=""
-                        category="cat-2"/>
-                        <PorfolioImage 
-                        img={require('../../Images/portfolio/photoshoots/blacklips/PurpleRose_PhotoshootMakeUp_UrbanDecay3.jpg')} 
-                        heading="Closeup"
-                        desc=""
-                        category="cat-2"/>
-                        <PorfolioImage 
-                        img={require('../../Images/portfolio/photoshoots/blacklips/PurpleRose_PhotoshootMakeUp_UrbanDecay5.jpg')} 
-                        heading="Lether Jacket"
+                        img={require('../../Images/portfolio/photoshoots/skin/PurpleRose_PhotoshootMakeUp_Skin_lauren-26.jpg')} 
+                        heading="Group"
                         desc=""
                         category="cat-3"/>
                         <PorfolioImage 
-                        img={require('../../Images/portfolio/photoshoots/blacklips/PurpleRose_PhotoshootMakeUp_UrbanDecay6.jpg')} 
-                        heading="Lether Jacke"
-                        desc=""
-                        category="cat-3"/>
-                        <PorfolioImage 
-                        img={require('../../Images/portfolio/photoshoots/blacklips/PurpleRose_PhotoshootMakeUp_UrbanDecay7.jpg')} 
-                        heading="Full Frame"
-                        desc=""
-                        category="cat-4"/>
-                        <PorfolioImage 
-                        img={require('../../Images/portfolio/photoshoots/blacklips/PurpleRose_PhotoshootMakeUp_UrbanDecay9.jpg')} 
-                        heading="Closeup"
+                        img={require('../../Images/portfolio/photoshoots/skin/PurpleRose_PhotoshootMakeUp_Skin_lauren-28.jpg')} 
+                        heading="Group Face"
                         desc=""
                         category="cat-2"/>
                         <PorfolioImage 
-                        img={require('../../Images/portfolio/photoshoots/blacklips/PurpleRose_PhotoshootMakeUp_UrbanDecay4.jpg')} 
-                        heading="Family"
+                        img={require('../../Images/portfolio/photoshoots/skin/PurpleRose_PhotoshootMakeUp_Skin_lauren-12.jpg')} 
+                        heading="Group"
+                        desc=""
+                        category="cat-3"/>
+                        <PorfolioImage 
+                        img={require('../../Images/portfolio/photoshoots/skin/PurpleRose_PhotoshootMakeUp_Skin_lauren-10.jpg')} 
+                        heading="Group"
+                        desc=""
+                        category="cat-3"/>
+                        <PorfolioImage 
+                        img={require('../../Images/portfolio/photoshoots/skin/PurpleRose_PhotoshootMakeUp_Skin_lauren-6.jpg')} 
+                        heading="Group"
+                        desc=""
+                        category="cat-2"/>
+                        <PorfolioImage 
+                        img={require('../../Images/portfolio/photoshoots/skin/PurpleRose_PhotoshootMakeUp_Skin_lauren-32.jpg')} 
+                        heading="Group Face"
                         desc=""
                         category="cat-5"/>
                     </Gallery>
