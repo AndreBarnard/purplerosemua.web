fffd511dec2ef8cf113743639e7e3bd8d7851b36
Andre Barnard <25427881+AndreBarnard@users.noreply.github.com>
2020-02-16 14:07:42 +0200

    Update service mainpage

--- src/Service/ServicesMain.tsx
+++ src/Service/ServicesMain.tsx
@@ -12,33 +12,48 @@ function ServicesMain(props : IProps) {
             <div className="section-content m-b50 ">
                 <div className="row">
                     <ServiceItem 
-                    name="Service 1"
-                    img={require("../Images/our-work/pic1.jpg")}
+                    name="Happily Ever After"
+                    img={require("../Images/our-work/PurpleRoseMUA_CapeTown_BridalMakeUp.jpg")}
+                    desc="Bridal Make up"
+                    />
+                   <ServiceItem 
+                    name="Natural Glow Up"
+                    img={require("../Images/our-work/PurpleRoseMUA_CapeTown_NatualGlowup.jpg")}
+                    desc="Some Text about service"
+                    />
+                    <ServiceItem 
+                    name="Glow Up"
+                    img={require("../Images/our-work/PurpleRoseMUA_CapeTown_Glowup.jpg")}
+                    desc="Some Text about service"
+                    />
+                    <ServiceItem 
+                    name="Threading and Waxing"
+                    img={require("../Images/our-work/PurpleRoseMUA_CapeTown_Waxing.jpg")}
                     desc="Some Text about service"
                     />
                    <ServiceItem 
-                    name="Service 1"
-                    img={require("../Images/our-work/pic1.jpg")}
+                    name="Tinting"
+                    img={require("../Images/our-work/PurpleRoseMUA_CapeTown_Tinting.jpg")}
                     desc="Some Text about service"
                     />
                     <ServiceItem 
-                    name="Service 1"
-                    img={require("../Images/our-work/pic1.jpg")}
+                    name="Lash Lifting"
+                    img={require("../Images/our-work/PurpleRoseMUA_CapeTown_LashLifting.jpg")}
                     desc="Some Text about service"
                     />
                     <ServiceItem 
-                    name="Service 1"
-                    img={require("../Images/our-work/pic1.jpg")}
-                    desc="Some Text about service"
-                    />
-                   <ServiceItem 
-                    name="Service 1"
-                    img={require("../Images/our-work/pic1.jpg")}
+                    name="Hair Styling"
+                    img={require("../Images/our-work/PurpleRoseMUA_CapeTown_HairStyling.jpg")}
                     desc="Some Text about service"
                     />
                     <ServiceItem 
-                    name="Service 1"
-                    img={require("../Images/our-work/pic1.jpg")}
+                    name="Special Effects"
+                    img={require("../Images/our-work/PurpleRoseMUA_CapeTown_SpecailEffects.jpg")}
+                    desc="Some Text about service"
+                    />
+                    <ServiceItem 
+                    name="Facepaint"
+                    img={require("../Images/our-work/PurpleRoseMUA_CapeTown_Facepainting.jpg")}
                     desc="Some Text about service"
                     />
                     
