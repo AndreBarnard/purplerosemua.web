c2a9ffd4c0ddc4d7d70fdb8d6341d837103db7c1
Andre Barnard <25427881+AndreBarnard@users.noreply.github.com>
2020-02-19 06:40:18 +0200

    Update services

--- src/Service/ServicesMain.tsx
+++ src/Service/ServicesMain.tsx
@@ -14,47 +14,46 @@ function ServicesMain(props : IProps) {
                     <ServiceItem 
                     name="Happily Ever After"
                     img={require("../Images/our-work/PurpleRoseMUA_CapeTown_BridalMakeUp.jpg")}
-                    desc="Bridal Make up"
+                    desc="Bridal make-up is usually natural, gorgeous and glowing, with soft defintion on the eyes and a natural lip. It is an essential part pf the wedding planning process to design the prefect look for your special day."
                     />
                    <ServiceItem 
                     name="Natural Glow Up"
                     img={require("../Images/our-work/PurpleRoseMUA_CapeTown_NatualGlowup.jpg")}
-                    desc="Some Text about service"
+                    desc="Natural make-up will enhance your deatures, rather than disquise flaws. It's a simple approach that focuseson a few key areas - skin, eyes and lips and bring out your best in a suble way."
                     />
                     <ServiceItem 
                     name="Glow Up"
                     img={require("../Images/our-work/PurpleRoseMUA_CapeTown_Glowup.jpg")}
-                    desc="Some Text about service"
+                    desc="A glow up make-up look will change your entire aura. By adding some glow, booting your brows and smoky eyes. While toping it with colourfull lips and amping up the volume. Finally setting your look for `just applied` makeup all day and night look."
                     />
                     <ServiceItem 
                     name="Threading and Waxing"
                     img={require("../Images/our-work/PurpleRoseMUA_CapeTown_Waxing.jpg")}
-                    desc="Some Text about service"
+                    desc="Threading is simply using cotton which is twisted in the centre to remove unwanted hair from the root. Waxing removes hair from the root by using a sticky substance, to adhere to body hair, and then removing this covering and pulling out the hair from the follicle."
                     />
                    <ServiceItem 
                     name="Tinting"
                     img={require("../Images/our-work/PurpleRoseMUA_CapeTown_Tinting.jpg")}
-                    desc="Some Text about service"
+                    desc="Tinting involves carefull dying the lashes/brows with high quality tints to givethem a natural, lusious, darker apperance. Many different eyelash tint colours ( including blues, browns, blacks and greys ) are available so we can compliment the skin and hair colouring prefectly."
                     />
                     <ServiceItem 
                     name="Lash Lifting"
                     img={require("../Images/our-work/PurpleRoseMUA_CapeTown_LashLifting.jpg")}
-                    desc="Some Text about service"
-                    />
+                    desc="Lash lifting is one of the easiest and fastest ways to brighten and lift the entire face. Its a stunning alternative to eyelash extensions and is on of the essential treatments chosen by many makeup artist for giving that wide-eyed look to your natural lash."/>
                     <ServiceItem 
                     name="Hair Styling"
                     img={require("../Images/our-work/PurpleRoseMUA_CapeTown_HairStyling.jpg")}
-                    desc="Some Text about service"
+                    desc="For short and sharp cuts, dignified updo styles, or long and luscious locks for your wedding, fashion shoot, or commercial – we’re experienced with all things hair."
                     />
                     <ServiceItem 
                     name="Special Effects"
                     img={require("../Images/our-work/PurpleRoseMUA_CapeTown_SpecailEffects.jpg")}
-                    desc="Some Text about service"
+                    desc="Special Effects Makeup can be defined as the use of prosthetic sculpting, molding, casting and painting to create advanced cosmetic effects.It is mostly popular in TV shows, theater, movies and even at Holloween"
                     />
                     <ServiceItem 
                     name="Facepaint"
                     img={require("../Images/our-work/PurpleRoseMUA_CapeTown_Facepainting.jpg")}
-                    desc="Some Text about service"
+                    desc="Face painting is the art of applying paint to a person's skin. Face painting is frequently composed of small decorative images applied to the face of a person although hands, arms and shoulders are popular locations."
                     />
                     
                 </div>
