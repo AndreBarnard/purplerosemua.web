0c7484dd3f530a5497dc30c1a96cb3d88540aecd
Andre Barnard <25427881+AndreBarnard@users.noreply.github.com>
2020-02-22 09:38:14 +0200

    Remove UnderConstruction

--- src/ContactUs/Contact.tsx
+++ src/ContactUs/Contact.tsx
@@ -1,15 +1,8 @@
 import * as React from 'react';
-import Underconstruction from '../Brochure/Underconstruction';
 import ContactUsContent from './ContactUsContent'
 
-export interface IAppProps {
-}
-
 function Contact() {
-
-    return <ContactUsContent />// <Underconstruction />   
-    
+    return <ContactUsContent />      
 }
-
 
 export default Contact
